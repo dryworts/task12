fe3947931f679756bfde27c67b3d161c97f19099
dryworts <omzg@mail.ru>
2023-08-24 12:30:47 +0600

    Tests Passed

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -1,2 +1,46 @@
 public class MovieManager {
+    //создаем пустой массив
+    private String[] movies = new String[0];
+    private int limit;
+    //создаем конструктор
+
+    public MovieManager() {
+        this.limit = 5;
+    }
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addMovie(String movie) {
+//создаем временный массив, но на единицу больше
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        this.movies = tmp;
+    }
+
+    //показать все фильмы
+    public String[] findAll() {
+        return movies;
+    }
+
+    //показать последние добавленные фильмы
+    public String[] findLast() {
+        //заводим переменную для вычисления последних фильмов, т.е. сколько надо показать
+        int resultLength;
+        if (movies.length < limit) {
+            resultLength = movies.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] answer = new String[resultLength];
+        for (int i = 0; i < answer.length; i++) {
+            //чтобы скопировать элемент с конца другого массива делай так:
+            answer[i] = movies[movies.length - 1 - i];
+        }
+        return answer;
+    }
 }
--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,2 +1,68 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MovieManagerTest {
+
+    @Test
+    //тестируем пустой массив
+    public void testEmpty(){
+        MovieManager manager = new MovieManager();
+
+        String[] actual = manager.findAll();
+        String[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    //тестируем добавление одного фильма
+    public void testOneMovie(){
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film I"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    //тестируем добавление одного фильма
+    public void testAddSomeMovies(){
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film I", "Film II", "Film III",};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    //тестируем показать в обратном порядке
+    public void testShowLastFilms(){
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+        String[] actual = manager.findLast();
+        String[] expected = {"Film III", "Film II", "Film I",};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    //тестируем показать в обратном порядке с конструктором с 3 фильмами
+    public void testShowThreeLastFilms(){
+        MovieManager manager = new MovieManager(3);
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+        String[] actual = manager.findLast();
+        String[] expected = {"Film III", "Film II", "Film I",};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
